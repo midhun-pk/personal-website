406f50f8ae1d1edfdbee03b81054cf2223cefbb3
midhun-pk <pkr.midhun@gmail.com>
2018-12-31 10:13:19 +0530

    Added content to experience module

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
+  activeTabIndex = 0;
+
+  jobs = [{
+    company: 'Infosys',
+    role: 'Engineer',
+    duration: 'Nov 2015 - Dec 2018',
+    descriptions: [
+      'Write modern, performant, maintainable code for a diverse array of client and internal projects',
+      'Work with a variety of different languages, platforms, frameworks, and content management systems \
+        such as JavaScript, TypeScript, Gatsby, React, Craft, Wordpress, Prismic, and Netlify',
+      'Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis'
+    ]
+  }, {
+    company: 'TCS',
+    role: 'Intern',
+    duration: 'July 2014 - Oct 2014',
+    descriptions: [
+      'Work with a variety of different languages, platforms, frameworks, and content management systems \
+        such as JavaScript, TypeScript, Gatsby, React, Craft, Wordpress, Prismic, and Netlify',
+      'Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis'
+    ]
+  }];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setActiveTab(index: number): void {
+    this.activeTabIndex = index;
+  }
+
+  isActive(index: number): boolean {
+    return this.activeTabIndex === index;
+  }
+
+  getHighlightTransform(): string {
+    if (window.innerWidth < 600) {
+      return 'translateX(' + (this.activeTabIndex > 0 ? this.activeTabIndex * 120 : 0) + 'px)';
+    }
+    return 'translateY(' + (this.activeTabIndex > 0 ? this.activeTabIndex * 42 : 0) + 'px)';
   }
 
 }
